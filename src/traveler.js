16143710dd3a7b044ef1386803648b0021a092c3
Oakley Windiate <oakley.windiate@gmail.com>
2022-03-05 11:00:33 -0700

    Create constructor for traveler

---
+++ src/traveler.js
@@ -0,0 +1,13 @@
+class Taveler {
+  constructor(client) {
+    this.past = client.past;
+    this.present = client.present;
+    this.upcoming = client.upcoming;
+    this.pending = client.pending;
+  }
+  calculateFees() {
+    
+  }
+}
+
+export default User;
